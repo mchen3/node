4c48c8333dca13fc8c78353fd1564ee3a850276d
first commit <tap135@hotmail.com>
2018-06-21 16:36:38 -0400

    JSON

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const http = require('http');
 const server = http.createServer((request, response) => {
 
   //console.log('headers', request.headers);
-  console.log('method', request.method);
-  console.log('url', request.url);
+  //console.log('method', request.method);
+  //console.log('url', request.url);
 
-  response.setHeader('Content-Type', 'text/html');
-  response.end('<h1>Heoo</h1>');
+  const user = {
+    name: 'John',
+    hobby: 'Riding'
+  }
+
+  response.setHeader('Content-Type', 'application/json');
+  response.end(JSON.stringify(user));
 
 })
 
