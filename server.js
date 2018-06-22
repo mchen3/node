ed1f24012269a955990aab9dc75f04e3de717cd5
first commit <tap135@hotmail.com>
2018-06-21 20:33:31 -0400

    Middleware

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 
+// Middleware 
+app.use(( req, res, next) => {
+  console.log('<h1> Hello </h1>');
+  next();
+})
+
 app.get('/', (req,res) => {
   res.send("getting root");
 });
