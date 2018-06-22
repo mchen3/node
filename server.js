7440e425d48368ad5fac3e4ed8effe7e4b7653d2
first commit <tap135@hotmail.com>
2018-06-22 16:16:25 -0400

    Testing static html files

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const app = express();
 
-app.use((bodyParser.urlencoded({extended: false})));
-app.use(bodyParser.json());
 
-app.get('/:id', (req,res) => {
+app.use(express.static(__dirname + '/public'))
 
-  //console.log(req.query);
-  //console.log(req.body);
-  // console.log(req.headers);
- console.log(req.params);
-
-  res.status(404).send("Not found");
-});
 
 app.listen(3000);
